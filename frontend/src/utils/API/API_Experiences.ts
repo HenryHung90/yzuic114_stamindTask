b46b3d0a54c1c6fa643752e36f9c6ffa0aac519b
HenryHung90 <hungwork510031@gmail.com>
2025-02-27 11:45:26 +0800

    change name

---
+++ frontend/src/utils/API/API_Experiences.ts
@@ -0,0 +1,21 @@
+import {API_GET, API_POST} from "./API_Config";
+import {Req_experienceInfo, Req_experienceFileInfo} from "./API_Interface";
+
+
+const API_getTaskExperience = (taskId: string | undefined) => {
+  const experienceData: Req_experienceInfo = {
+    task_id: taskId,
+  }
+  return new API_POST(import.meta.env.VITE_APP_API_GET_TASK_EXPERIENCE || '', experienceData).sendRequest()
+}
+
+const API_uploadTaskExperienceFile = (taskId: string, file: File, selectNode: number) => {
+  const formData = new FormData()
+  formData.append('file', file)
+  formData.append('task_id', taskId)
+  formData.append('select_node',selectNode.toString())
+  console.log(selectNode.toString())
+  return new API_POST(import.meta.env.VITE_APP_API_UPLOAD_EXPERIENCE_FILE || '', formData).sendRequest()
+}
+
+export {API_getTaskExperience, API_uploadTaskExperienceFile}
