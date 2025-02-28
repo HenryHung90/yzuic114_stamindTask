f63271d2a383c7b612b5ed72c2210123a66943cd
HenryHung90 <hungwork510031@gmail.com>
2025-02-28 23:34:31 +0800

    add key index for subTargetList

--- frontend/src/pages/Task/components/Target.tsx
+++ frontend/src/pages/Task/components/Target.tsx
@@ -40,7 +40,7 @@ const TargetComponent = (props: ITaskContentProps) => {
       </div>
       <div className='overflow-scroll flex flex-col gap-y-5 mt-5 pt-4 w-[60%] h-96 border-stamindTask-black-850'>
         {subTargetList.map(({title, description}, index) => (
-          <div className='flex flex-col min-h-44 p-4 gap-y-3 border-2 border-stamindTask-black-600 rounded-2xl'>
+          <div key={index} className='flex flex-col min-h-44 p-4 gap-y-3 border-2 border-stamindTask-black-600 rounded-2xl'>
             <h3 className='text-[1.5rem]'>🟢 子目標：{title}</h3>
             <p>{description}</p>
           </div>
