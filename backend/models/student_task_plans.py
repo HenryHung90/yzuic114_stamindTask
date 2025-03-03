502be3f6ff4c7c779e77f80ad0c07882ce4a8655
HenryHung90 <hungwork510031@gmail.com>
2025-03-03 19:56:57 +0800

    change task plan content to be select_sub_list & plan_list

--- backend/models/student_task_plans.py
+++ backend/models/student_task_plans.py
@@ -1,12 +1,20 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
 class StudentTaskPlan(models.Model):
-    content = models.JSONField(
-        default=dict,
+    select_sub_list = models.JSONField(
         null=True,
         blank=True,
-        verbose_name='任務計畫設定內容'
+        verbose_name='子任務的選擇',
+        help_text='包含是否選擇子任務的 Boolean 的 Array<JSON> 格式資料'
+    )
+
+    plan_list = models.JSONField(
+        null=True,
+        blank=True,
+        verbose_name='子任務',
+        help_text='包含 strategy, description 和 time 的 Array<JSON> 格式資料'
     )
 
     # 時間戳記
