eccdeea79f48248388542ee9619e4b811c8b16bc
HenryHung90 <hungwork510031@gmail.com>
2025-03-06 16:36:41 +0800

    add Process.tsx Component into TaskContent.tsx

--- frontend/src/pages/admin/task/components/TaskContent.tsx
+++ frontend/src/pages/admin/task/components/TaskContent.tsx
@@ -12,12 +12,12 @@ import {
 
 // components
 import ExperiencePageComponent from "./Experience";
-import {ISettingAlertLogAndLoading} from "../../../../utils/interface/alertLog";
+import TargetComponent from "./Target";
+import ProcessComponent from "./Process";
 import {calculateExperienceStep} from "../../../../utils/functions/tasks/experience";
 
 // interface
 import {ITaskContentProps} from "../../../../utils/interface/Task";
-import TargetComponent from "./Target";
 
 const TaskContentComponent = (props: ITaskContentProps) => {
   const {taskId, selectNode, settingAlertLogAndLoading} = props;
@@ -51,6 +51,10 @@ const TaskContentComponent = (props: ITaskContentProps) => {
         {selectNode.category === 'Target' &&
             <TargetComponent taskId={taskId} selectNode={selectNode} savingTrigger={savingTrigger}
                              settingAlertLogAndLoading={settingAlertLogAndLoading}/>}
+        {selectNode.category === 'Process' &&
+            <ProcessComponent taskId={taskId} selectNode={selectNode}
+                              settingAlertLogAndLoading={settingAlertLogAndLoading}/>
+        }
       </DialogBody>
       <DialogFooter placeholder={undefined} className='gap-x-2'>
         {selectNode.category === 'Target' &&
