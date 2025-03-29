fe5c96a8ff0a321da07bebecce21c726c24e2151
HenryHung90 <hungwork510031@gmail.com>
2025-03-29 10:30:12 +0800

    add: create the table for student_record

---
+++ frontend/src/utils/listener/speedDial.ts
@@ -0,0 +1,13 @@
+const speedDialTable: Record<string, string> = {
+  'openTextBook': '開啟教材',
+  'closeTextBook': '關閉教材',
+  'openNoteBook': '開啟筆記',
+  'closeNoteBook': '關閉筆記',
+  'openChatRoom': '開啟聊天室',
+  'closeChatRoom': '關閉聊天室',
+  'chatAmumamum': 'AI 聊天輸入框',
+  'submitChat': '傳送訊息',
+  'saveNoteBook': '儲存筆記'
+}
+
+export default speedDialTable
---
+++ frontend/src/utils/listener/task.ts
@@ -0,0 +1,40 @@
+const taskTable: Record<string, string> = {
+  'enterTarget': '進入學習目標',
+  'enterExperience': '進入體驗任務',
+  'enterPlan': '進入計畫設定',
+  'enterProcess': '進入計劃執行',
+  'enterReflection': '進入自我反思',
+  'leaveTarget': '離開學習目標',
+  'leaveExperience': '離開體驗任務',
+  'leavePlan': '離開計畫設定',
+  'leaveProcess': '離開計劃執行',
+  'leaveReflection': '離開自我反思',
+  // target
+  'target': '學習目標',
+  'targetDescription': '學習目標描述',
+  'subTarget': '學習子目標',
+  'subTargetDescription': '學習子目標描述',
+  // plan
+  'plan': '計畫設定',
+  'addPlan': '新增策略',
+  'removePlan': '刪除策略',
+  'changePlanstrategy': '變更策略種類',
+  'changePlandescription': '變更策略描述',
+  'changePlantime': '變更策略時間',
+  // process
+  'processhtml': 'HTML 程式區',
+  'processcss': 'CSS 程式區',
+  'processjavascript': 'JS 程式區',
+  'processFontIncrease': '增大程式字體',
+  'processFontDecrease': '縮小程式字體',
+  'processSave': '儲存當前程式',
+  'processOpenIframe': '打開程式執行預覽',
+  'processCloseIframe': '關閉程式執行預覽',
+  'processCodeEditor': '程式區塊',
+  //reflection
+  'saveReflection': '儲存反思內容',
+  'changeReflection': '更改反思內容',
+}
+
+
+export default taskTable
