e5ecc79475bdd75855d496438bb2c525af0d70e0
HenryHung90 <hungwork510031@gmail.com>
2025-03-29 10:27:17 +0800

    refactor: student_record revision

--- backend/models/student_record.py
+++ backend/models/student_record.py
@@ -1,8 +1,18 @@
+from tabnanny import verbose
+
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
 class StudentRecord(models.Model):
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='student_records',
+        verbose_name='學生',
+    )
+
     class_name = models.ForeignKey(
         'ClassName',
         on_delete=models.SET_NULL,
@@ -12,23 +22,48 @@ class StudentRecord(models.Model):
         verbose_name="班級"
     )
 
-    # 'action_name': '',
-    # 'action_id': '',
-    # 'verb': '',
-    # 'object_type': '',
-    # 'object_id': '',
-    # 'object_name': '',
-    # 'context': ''
-    action_record = ArrayField(
-        models.JSONField(
-            default=dict,
-            null=True,
-            blank=True
-        ),
+    verb = models.CharField(
+        max_length=50,
+        default="",
+        verbose_name="行為動詞"
+    )
+
+    time = models.CharField(
+        max_length=50,
+        default="",
+        verbose_name='行為發生時間'
+    )
+
+    timer = models.CharField(
+        max_length=50,
         null=True,
         blank=True,
-        verbose_name="記錄內容"
+        verbose_name='時間經過'
     )
+
+    object_type = models.CharField(
+        max_length=50,
+        default="",
+        verbose_name='操作對象類型'
+    )
+
+    object_name = models.CharField(
+        max_length=100,
+        default="",
+        verbose_name='操作對象名稱'
+    )
+
+    object_id = models.CharField(
+        max_length=100,
+        default="",
+        verbose_name='操作對象 id'
+    )
+
+    context = models.JSONField(
+        default=dict,
+        verbose_name='行為上下文敘述以及完整訊息'
+    )
+
 
     # 時間戳記
     created_at = models.DateTimeField(auto_now_add=True)
