ae6d5208301b6905839c5dd115f9454ac2edbdff
henryhung90 <hungwork510031@gmail.com>
2025-04-09 13:39:45 +0800

    refactor: improve urls.py and change vite.config.ts to adjust production and development

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,19 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg', '**/*.gif'],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'static', // 將靜態資源放入 static 資料夾
-    emptyOutDir: true,
-  },
+export default defineConfig(({ command, mode }) => {
+  // 判斷當前環境
+  const isProd = mode === 'production';
+  
+  return {
+    plugins: [react()],
+    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg', '**/*.gif'],
+    // 根據環境設置不同的 base
+    base: isProd ? '/taskmind/' : '/',
+    build: {
+      outDir: 'dist',
+      assetsDir: 'static', // 將靜態資源放入 static 資料夾
+      emptyOutDir: true,
+    },
+  }
 })
