aa2f62f9b6c2872d7b440adeda87301e451f08bb
HenryHung90 <hungwork510031@gmail.com>
2025-04-22 16:53:25 +0800

    feat(utils): move XLSX file conversion function to common functions(version/1.1.0)

--- frontend/src/utils/functions/common.ts
+++ frontend/src/utils/functions/common.ts
@@ -1,3 +1,5 @@
+import * as XLSX from 'xlsx'
+
 export enum EGroupType {
   NONE = 'NONE',
   EXPERIMENTAL = 'EXPERIMENTAL',
@@ -5,9 +7,32 @@ export enum EGroupType {
 }
 
 
-
-function calculateTimer(startTime: number){
+function calculateTimer(startTime: number) {
   return Math.floor((Date.now() - startTime) / 1000).toString()
 }
 
-export {calculateTimer}
+function convertToXlsxFile(sheetName: string, workbookName: string, data: []) {
+  // 將數據轉換為工作表
+  const worksheet = XLSX.utils.json_to_sheet(data);
+
+  // 創建工作簿並添加工作表
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, workbookName);
+
+  // 將工作簿轉換為二進制數據
+  const workbookBinary = XLSX.write(workbook, {bookType: 'xlsx', type: 'array'});
+
+  // 創建 Blob 對象
+  const blob = new Blob([workbookBinary], {type: 'application/octet-stream'});
+
+  // 創建下載鏈接並觸發下載
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = sheetName;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+export {calculateTimer, convertToXlsxFile}
