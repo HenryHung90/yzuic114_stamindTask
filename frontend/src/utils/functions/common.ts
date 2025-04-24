60f615bef6d4c1c1f16f00a994e4617a9305a079
HenryHung90 <hungwork510031@gmail.com>
2025-04-24 22:28:28 +0800

    feat(utils): improve convertToXlsxFile function
    - Refactor convertToXlsxFile function to handle multiple datasets and
      corresponding workbook names. Each dataset is now converted to a
      worksheet and appended to the workbook with the respective name.
    
    (issue: version/1.1.0)

--- frontend/src/utils/functions/common.ts
+++ frontend/src/utils/functions/common.ts
@@ -11,13 +11,17 @@ function calculateTimer(startTime: number) {
   return Math.floor((Date.now() - startTime) / 1000).toString()
 }
 
-function convertToXlsxFile(sheetName: string, workbookName: string, data: []) {
-  // 將數據轉換為工作表
-  const worksheet = XLSX.utils.json_to_sheet(data);
-
+function convertToXlsxFile(sheetName: string, workbookNames: Array<string>, data: Array<Array<JSON>>) {
   // 創建工作簿並添加工作表
   const workbook = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(workbook, worksheet, workbookName);
+
+  for (let i = 0; i < workbookNames.length; i++) {
+    // Convert each dataset to a worksheet
+    const worksheet = XLSX.utils.json_to_sheet(data[i]);
+    // Append the worksheet to the workbook with the corresponding name
+    XLSX.utils.book_append_sheet(workbook, worksheet, workbookNames[i]);
+  }
+
 
   // 將工作簿轉換為二進制數據
   const workbookBinary = XLSX.write(workbook, {bookType: 'xlsx', type: 'array'});
